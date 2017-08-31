d550cfe85056cf502f010a5fed2ef61e65c3ca10
nathan-beneke <benek020@morris.umn.edu>
2017-08-31 11:18:04 -0500

    re-tars the cleaned file into the correct place

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -2,6 +2,14 @@
 
 tar -xf $1 -C $2 # untars the specified file into the specified place
 
+path=`pwd` # stores the file path as a variable
+
 cd $2
 
+#searches for and removes files containing "DELETE ME!"
 grep -lrZ "DELETE ME!" . | xargs -0 rm -f --
+
+path=${path}"/cleaned_"$1 #sets the new path
+
+tar -zcf $path */*
+
